ee5f02bc04717af9da9b33708cf7f9a885b10057
Renata Negrini <renatanegrinim@gmail.com>
2025-06-16 11:32:09 -0300

    :tada: Commit Inicial

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,32 @@
+
+function Home() {
+  return (
+    <div style={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100vw"
+    }}>
+        <div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "80vw"
+            }}>
+                <h2>Seja Bem Vinde!</h2>
+                <p>Expresse aqui seus pensamentos e opiniões</p>
+            </div>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                width: "80vw"
+            }}>
+                <img src="https://i.imgur.com/VpwApCU.png" alt="Imagem da Página Inicial" width="400px"/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
